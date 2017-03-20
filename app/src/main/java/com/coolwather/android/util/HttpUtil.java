6d4fb49ca9845ca036ff49fe1c6a426d98dff2f8
chenyuchong <781893621@qq.com>
2017-03-20 18:10:20 +0800

    完成遍历省市县三级列表的功能

---
+++ app/src/main/java/com/coolwather/android/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.coolwather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by chenyuchong on 2017/3/20.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
+        OkHttpClient client =new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
